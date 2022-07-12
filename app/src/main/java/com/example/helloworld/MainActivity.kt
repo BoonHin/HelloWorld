1e27f5eacb7f88c0e6a00ab4e64b42e85ad479d4
BoonHin <wenqian900@gmail.com>
2022-07-12 17:50:12 +0800

    Completed

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,16 +13,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.rollButton)
+        //OR val rollButton = findViewById<Button>(R.id.rollBtn)
         rollButton.setOnClickListener{ rollDice() }
     }
 
     private fun rollDice(){
         Toast.makeText(this, "Roll !",
-            Toast.LENGTH_LONG).show()
-
+            Toast.LENGTH_SHORT).show()
 
         val randomNum = (1..6).random()
-
+        val diceImage: ImageView = findViewById(R.id.diceImg)
+        val imgSrc = when(randomNum){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(imgSrc)
         val numberText: TextView = findViewById(R.id.numberText)
         numberText.text = randomNum.toString()
     }
