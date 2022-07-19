c37912df66a6ffdf13b35f780c4a65eca0397a49
BoonHin <wenqian900@gmail.com>
2022-07-19 17:58:51 +0800

    Completed 19/7/2022

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.example.helloworld
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImage:ImageView //var = value can be changed, not a constant
     lateinit var numberText: TextView
+    lateinit var editPlayerTxt: EditText
+    lateinit var playerNameTxt: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editPlayerTxt = findViewById(R.id.editPlayerNameET)
+        playerNameTxt = findViewById(R.id.playerName)
+        val updateBtn: Button = findViewById(R.id.updateButton)
+        updateBtn.setOnClickListener{updateName(it)}
 
         diceImage = findViewById(R.id.diceImg)
         numberText = findViewById(R.id.numberText)
         val rollButton: Button = findViewById(R.id.rollButton)
         //OR val rollButton = findViewById<Button>(R.id.rollBtn)
         rollButton.setOnClickListener{ rollDice() }
+    }
+
+    private fun updateName(view : View){
+        playerNameTxt.text = editPlayerTxt.text
+
+        editPlayerTxt.text.clear()
+        editPlayerTxt.clearFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     private fun rollDice(){
